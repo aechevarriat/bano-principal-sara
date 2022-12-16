30024b1d47c9b32c1dfe97c6d4f973a47c33e711
aechevarriat <aliciaechevarria27@gmail.com>
2022-12-16 07:32:45 +0100

    Initial commit

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bao_1effectsresult",
+      "name": "baño_1.effectsResult",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.4654444844336094,
+        "pitch": 0.3341718365980295,
+        "fov": 1.41569238083293
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.292095495441199,
+          "pitch": 0.7477524587402051,
+          "rotation": 6.283185307179586,
+          "target": "1-bao_2effectsresult"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bao_2effectsresult",
+      "name": "baño_2.effectsResult",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.133931124822535,
+          "pitch": 0.37922086880794836,
+          "rotation": 0.7853981633974483,
+          "target": "0-bao_1effectsresult"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Baño_Sara",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
